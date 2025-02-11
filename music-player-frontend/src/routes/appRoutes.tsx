95c6ad404dcde8b9816000df0d0d2864f9d4345b
Niyam Singhvi <niyam19@gmail.com>
2025-02-11 22:03:54 +0530

    profile page route added

--- music-player-frontend/src/routes/appRoutes.tsx
+++ music-player-frontend/src/routes/appRoutes.tsx
@@ -1,11 +1,13 @@
 import Home from "../pages/Home";
 import { Route, Routes } from "react-router-dom";
 import LikedSongs from "../pages/LikedSongs";
+import ProfilePage from "../pages/profile";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/liked-songs" element={<LikedSongs />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   );
 };
