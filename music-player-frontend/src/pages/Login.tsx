eadec1332ef120882875231cc3b47f2fdc7c605b
Niyam Singhvi <niyam19@gmail.com>
2025-02-15 18:58:21 +0530

    login and signup page UI modifications

--- music-player-frontend/src/pages/Login.tsx
+++ music-player-frontend/src/pages/Login.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { API_URL } from '../constants/apiEnum';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { API_URL } from "../constants/apiEnum";
+import Icon from "../assets/icons/music-player.png";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   const navigate = useNavigate();
 
@@ -16,67 +17,89 @@ const Login = () => {
 
     try {
       const response = await fetch(`${API_URL}/auth/login`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
 
       const result = await response.json();
       if (response.ok) {
-        localStorage.setItem('token', result.token);
-        localStorage.setItem('userData', JSON.stringify(result.user));
-        setMessage('Login successful!');
+        localStorage.setItem("token", result.token);
+        localStorage.setItem("userData", JSON.stringify(result.user));
+        setMessage("Login successful!");
         setTimeout(() => {
-            navigate('/',{replace: true})
-            // window.location.reload();
+          navigate("/", { replace: true });
+          // window.location.reload();
         }, 500);
       } else {
-        setMessage(result.message || 'Login failed.');
+        setMessage(result.message || "Login failed.");
       }
     } catch (error) {
-      setMessage('An error occurred. Please try again.');
+      setMessage("An error occurred. Please try again.");
     }
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-white shadow-md rounded-md">
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
-      <form onSubmit={handleLogin}>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="w-full p-2 border rounded-md"
-          />
+    <div className="bg-black h-screen flex flex-col items-center justify-center p-6">
+      <div className="mb-6 text-center">
+        <img src={Icon} alt="Music Player Logo" className="h-12 mx-auto mb-2" />
+        <h1 className="text-3xl font-bold text-white">Music Player</h1>
+      </div>
+      <div className="w-full max-w-md p-8 bg-zinc-900 shadow-lg rounded-lg">
+        <h2 className="text-2xl font-bold mb-6 text-white text-center">Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-zinc-400"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full p-3 rounded-md bg-zinc-700 text-white"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-zinc-400"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full p-3 rounded-md bg-zinc-700 text-white"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded-md"
+          >
+            Login
+          </button>
+        </form>
+        {message && (
+          <p className="mt-4 text-center text-sm text-zinc-300">{message}</p>
+        )}
+        <div className="mt-4 text-center">
+          <p className="text-base text-white">
+            Don't have an account?{" "}
+            <Link to="/signup" replace className="text-blue-500">
+              Sign Up
+            </Link>
+          </p>
         </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="w-full p-2 border rounded-md"
-          />
-        </div>
-        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md">
-          Login
-        </button>
-      </form>
-      {message && <p className="mt-4 text-center text-sm text-gray-600">{message}</p>}
-      <div className="mt-4 text-center">
-        <p className="text-sm">Don't have an account? <Link to="/signup" replace className="text-blue-500">Sign Up</Link></p>
       </div>
     </div>
   );
--- music-player-frontend/src/pages/SignUp.tsx
+++ music-player-frontend/src/pages/SignUp.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../constants/apiEnum";
+import Icon from "../assets/icons/music-player.png";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -48,93 +49,99 @@ const SignUp = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-white shadow-md rounded-md">
-      <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-      <form onSubmit={handleSignUp}>
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            className="w-full p-2 border rounded-md"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="w-full p-2 border rounded-md"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="w-full p-2 border rounded-md"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-            className="w-full p-2 border rounded-md"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white p-2 rounded-md"
-        >
-          Sign Up
-        </button>
-      </form>
-      {message && (
-        <p className="mt-4 text-center text-base text-gray-600">{message}</p>
-      )}
-      <div className="mt-4 text-center">
-        <p className="text-gray-600">
-          Already have an account?{" "}
+    <div className="bg-black h-screen flex flex-col items-center justify-center p-6">
+      <div className="mb-6 text-center">
+        <img src={Icon} alt="Music Player Logo" className="h-12 mx-auto mb-2" />
+        <h1 className="text-3xl font-bold text-white">Music Player</h1>
+      </div>
+      <div className="w-full max-w-md p-8 bg-zinc-900 shadow-lg rounded-lg">
+        <h2 className="text-2xl font-bold mb-6 text-white text-center">Sign Up</h2>
+        <form onSubmit={handleSignUp}>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block mb-1 text-sm font-medium text-zinc-400"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="w-full p-3 rounded-md bg-zinc-700 text-white"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block mb-1 text-sm font-medium text-zinc-400"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full p-3 rounded-md bg-zinc-700 text-white"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block mb-1 text-sm font-medium text-zinc-400"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full p-3 rounded-md bg-zinc-700 text-white"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block mb-1 text-sm font-medium text-zinc-400"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              className="w-full p-3 rounded-md bg-zinc-700 text-white"
+            />
+          </div>
           <button
-            onClick={() => navigate("/login")}
-            className="text-blue-500 underline"
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded-md"
           >
-            Login
+            Sign Up
           </button>
-        </p>
+        </form>
+        {message && (
+          <p className="mt-4 text-center text-sm text-zinc-300">{message}</p>
+        )}
+        <div className="mt-4 text-center">
+          <p className="text-white">
+            Already have an account?{" "}
+            <button
+              onClick={() => navigate("/login")}
+              className="text-blue-500"
+            >
+              Login
+            </button>
+          </p>
+        </div>
       </div>
     </div>
   );
